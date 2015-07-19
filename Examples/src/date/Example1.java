39c1fa2211ba92a534c9d0e0d4e9e06e4010bd78
Igor Nesterenya <nesterione@gmail.com>
2015-07-19 16:41:06 +0300

    Add date examples

---
+++ Examples/src/date/Example1.java
@@ -0,0 +1,41 @@
+package date;
+
+import javax.xml.crypto.Data;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+/**
+ * Created by igor on 16.07.2015.
+ */
+public class Example1 {
+    public static void main(String[] args) throws ParseException {
+
+        Date data = new Date();
+
+
+/*
+        System.out.println( data.toLocaleString() );
+        Locale.setDefault(Locale.US);
+        System.out.println( data.toLocaleString() );
+        Date s = new Date(347583485);
+*/
+
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+        String utc = simpleDateFormat.format(data);
+
+        Date d2 = simpleDateFormat.parse(utc);
+
+        System.out.println( utc );
+
+        /*SimpleDateFormat simpleDateFormat1 = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+        Date d2 = simpleDateFormat1.parse(utc);
+        System.out.println(d2);*/
+
+        /*Date d2 = simpleDateFormat.parse("15.08.12");
+        System.out.println(d2);*/
+
+
+    }
+}
