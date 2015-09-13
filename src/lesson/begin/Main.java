3a14e197d980f48bb2cd90840009eabbc0b02e6a
Igor Nesterenya <nesterione@gmail.com>
2015-09-13 18:26:46 +0300

    Test

---
+++ src/lesson/begin/Main.java
@@ -0,0 +1,24 @@
+package lesson.begin;
+
+import java.util.Random;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+
+        String f="";
+
+        String ii = Integer.toString(1);
+        String d = ii;
+        //String f = "java "+ 4;
+        String f2 = " sfsdf";
+        String s = f + f2.toUpperCase();
+
+        System.out.println(  s   );
+
+
+
+    }
+}
